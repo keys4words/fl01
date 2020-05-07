fa07f73f1ac5526f60317d328599885123f405db
keys4words <keys4words@gmail.com>
2020-05-08 00:32:59 +0300

    add api with weatheronline

---
+++ weather.py
@@ -0,0 +1,24 @@
+import requests
+
+def weather_by_city(city_name):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    params = {
+        "key": "1f12cc67ad5642faaee205719200705",
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 5,
+        "lang": "ru"
+    }
+    res = requests.get(weather_url, params=params)
+    weather = res.json()
+    if 'data' in weather:
+        if 'current_condition' in weather['data']:
+            try:
+                return weather['data']['current_condition'][0]
+            except (IndexError, TypeError):
+                return False
+    return False
+
+if __name__ == '__main__':
+    w = weather_by_city('Moscow, Russia')
+    print(w)
