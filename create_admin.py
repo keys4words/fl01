7fd405a9375dd9e9360617a42f2bf24c00f95489
keys4words <keys4words@gmail.com>
2020-06-04 13:39:44 +0300

    add authorizing

---
+++ create_admin.py
@@ -0,0 +1,27 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app
+from webapp.model import db, User
+
+app = create_app()
+
+with app.app_context():
+    username = input('Введите имя: ')
+
+    if User.query.filter(User.username == username).count():
+        print('Пользователь с таким именем уже существует!')
+        sys.exit(0)
+    
+    pass1 = getpass('Введите пароль: ')
+    pass2 = getpass('Повторите пароль: ')
+    if not pass1 == pass2:
+        print('Пароли отличаются!!!')
+        sys.exit(0)
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(pass1)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print('Пользователь с id {} успешно создан'.format(new_user.id))
