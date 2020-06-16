b4db4779352849114f8ee1ef05b13bdaf3016651
keys4words <keys4words@gmail.com>
2020-06-16 14:56:42 +0300

    add validation 4 forms

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,8 +1,12 @@
-from flask import abort, Blueprint, current_app, render_template
+from flask import abort, Blueprint, flash, current_app, redirect, render_template, request, url_for
 
+from flask_login import current_user, login_required
+
+from webapp.db import db
 from webapp.weather import weather_by_city
-from webapp.news.models import News
+from webapp.news.models import News, Comment
 from webapp.news.forms import CommentForm
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('news', __name__)
 
@@ -24,5 +28,20 @@ def single_news(news_id):
     return render_template('news/single_news.html', page_title=my_news.title, news=my_news, comment_form=comment_form)
 
 @blueprint.route('/news/comment', methods=['POST'])
+@login_required
 def add_comment():
-    pass
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(text=form.comment_text.data, news_id=form.news_id.data, user_id=current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Комментарий успешно добавлен')
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в поле {}: {}'.format(
+                    getattr(form, field).label.text,
+                    errors
+                ))
+    
+    return redirect(get_redirect_target())
